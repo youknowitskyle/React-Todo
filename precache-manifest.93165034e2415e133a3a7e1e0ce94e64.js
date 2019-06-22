2d3d14ce15bb2de7bedc62fcef43376252e9dd1d
Kyle Wong <lemondeer123@gmail.com>
2019-06-22 14:47:42 -0700

    Updates

---
+++ precache-manifest.93165034e2415e133a3a7e1e0ce94e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a602ce0e6702a01b1976cba959ec2a",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "ac5b2b5dbbcdf5fcb1cf",
+    "url": "/React-Todo/static/css/main.d1bfedce.chunk.css"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/React-Todo/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "ac5b2b5dbbcdf5fcb1cf",
+    "url": "/React-Todo/static/js/main.7f58313d.chunk.js"
+  },
+  {
+    "revision": "8cccfb14f8793be292ed",
+    "url": "/React-Todo/static/js/runtime~main.58a39a8a.js"
+  }
+]);
